b75a0213cea71bfd5192779ed3150fa3ce5ee879
Thomas Wray <twray95@gmail.com>
2024-01-24 10:36:56 -0700

    dynamically create journalPosts, some bugs made me reformat file structure

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-const {
-  JournalPost,
-  Comment,
-  createJournalEntry,
-} = require("./lib/journalPost");
-const journalForm = document.getElementById("journalForm");
+// import { JournalPost, Comment, createJournalEntry } from "./lib/journalPost";
+// const journalForm = document.getElementById("journalForm");
 
-console.log(JournalPost);
-console.log(Comment);
+// console.log(JournalPost);
+// console.log(Comment);
 
-const post1 = new JournalPost(
-  "Thomas",
-  "Test Post Creation",
-  "I am just testing the JournalPost class"
-);
+// const post1 = new JournalPost(
+//   "Thomas",
+//   "Test Post Creation",
+//   "I am just testing the JournalPost class"
+// );
 
-const comment1 = new Comment("Wray", "This is a comment", 5);
+// const comment1 = new Comment("Wray", "This is a comment", 5);
 
-post1.addComment(comment1);
+// post1.addComment(comment1);
 
-console.log(post1);
+// console.log(post1);
+
+createJournalEntry = () => {
+  let authName = document.getElementById("authName");
+  let journalTitle = document.getElementById("title");
+  let journalText = document.getElementById("text");
+  let parent = document.getElementById("journalList");
+  let newJournalTitle = document.createElement("h4");
+  let newJournalBody = document.createElement("p");
+
+  newJournalTitle.textContent = `${journalTitle.value} by ${authName.value}`;
+  newJournalBody.textContent = `${journalText.value}`;
+
+  console.log(newJournalTitle);
+
+  parent.appendChild(newJournalTitle);
+  parent.appendChild(newJournalBody);
+
+  authName.value = "";
+  journalTitle.value = "";
+  journalText.value = "";
+};
 
 journalForm.addEventListener("submit", (e) => {
   e.preventDefault();
